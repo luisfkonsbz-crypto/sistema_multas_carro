b248e3b13be97db5e5755627803ebe46d72a64e6
Luis Fernando <luis.bezerra2.estudante.ifms.edu.com>
2026-09-01 20:56:19 -0300

    refactor(controller): definir modelAndview para retornar dados da model carro para o html listarcarro

--- src/main/java/br/edu/ifms/fine_system/controller/CarroController.java
+++ src/main/java/br/edu/ifms/fine_system/controller/CarroController.java
@@ -1,13 +1,29 @@
 package br.edu.ifms.fine_system.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.ifms.fine_system.model.Carro;
 
 @Controller
 public class CarroController {
 
 	@GetMapping("/carros")
-	public String listar() {
-		return "ListaCarro";
+	public ModelAndView listarCarros() {
+		ModelAndView mv = new ModelAndView("ListaCarros");
+		mv.addObject("carros", buscarCarros());
+		return mv;
+	}
+	
+	public List<Carro> buscarCarros() {
+		// Aqui você pode implementar a lógica para buscar os carros do banco de dados
+		// Por enquanto, vamos retornar uma lista de exemplo
+		List<Carro> carros = new ArrayList<>();
+		carros.add(new Carro(1L, "Modelo A", "ABC-1234", "Vermelho"));
+		return carros;
 	}
 }
--- src/main/java/br/edu/ifms/fine_system/model/Carro.java
+++ src/main/java/br/edu/ifms/fine_system/model/Carro.java
@@ -1,5 +1,33 @@
 package br.edu.ifms.fine_system.model;
 
-public class Carro {
+import java.io.Serializable;
+
+public class Carro implements Serializable {
+	private Long id;
+	private String placa;
+
+	public Carro() {
+	}
+
+	public Carro(Long id, String modelo, String placa, String cor) {
+		this.id = id;
+		this.placa = placa;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
 
 }
